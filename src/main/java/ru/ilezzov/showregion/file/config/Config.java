c2703b5f175fa7ade71ac923057c0fb6f6b5d246
ilezzov-code <ilezov.morozov@yandex.com>
2025-07-08 00:32:08 +0300

    Add new param in config.yml: owner_count

--- src/main/java/ru/ilezzov/showregion/file/config/Config.java
+++ src/main/java/ru/ilezzov/showregion/file/config/Config.java
@@ -1,7 +1,5 @@
 package ru.ilezzov.showregion.file.config;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.configuration.ConfigurationSection;
 import ru.ilezzov.showregion.file.PluginFile;
 
@@ -19,6 +17,7 @@ public class Config {
 
     public void reload() {
         setValues();
+        showingSection.reload();
     }
 
     public String language() {
@@ -29,10 +28,6 @@ public class Config {
         return checkUpdates;
     }
 
-    public PluginFile configFile() {
-        return configFile;
-    }
-
     public ConfigShowingSection showingSection() {
         return showingSection;
     }
@@ -41,7 +36,15 @@ public class Config {
         private boolean defaultEnable;
         private boolean enableBossBar;
         private boolean enableActionBar;
+        private int ownerCount;
         private long tickRate;
+
+        private final PluginFile configFile;
+
+        public ConfigShowingSection(final PluginFile configFile) {
+            this.configFile = configFile;
+            setValues();
+        }
 
         public boolean defaultEnable() {
             return defaultEnable;
@@ -55,22 +58,33 @@ public class Config {
             return enableActionBar;
         }
 
+        public int ownerCount() {
+            return ownerCount;
+        }
+
         public long tickRate() {
             return tickRate;
+        }
+
+        public void reload() {
+            setValues();
+        }
+
+        private void setValues() {
+            final ConfigurationSection configurationSection = configFile.getConfig().getConfigurationSection("showing_settings");
+            assert configurationSection != null;
+
+            defaultEnable = configurationSection.getBoolean("default_enable");
+            enableBossBar = configurationSection.getBoolean("enable_boss_bar");
+            enableActionBar = configurationSection.getBoolean("enable_action_bar");
+            ownerCount = configurationSection.getInt("owner_count");
+            tickRate = configurationSection.getLong("tick_rate");
         }
     }
 
     private void setValues() {
         this.language = configFile.getString("language");
         this.checkUpdates = configFile.getBoolean("check_updates");
-
-        final ConfigurationSection configurationSection = configFile.getConfig().getConfigurationSection("showing_settings");
-        assert configurationSection != null;
-
-        this.showingSection = new ConfigShowingSection();
-        this.showingSection.defaultEnable = configurationSection.getBoolean("default_enable");
-        this.showingSection.enableBossBar = configurationSection.getBoolean("enable_boss_bar");
-        this.showingSection.enableActionBar = configurationSection.getBoolean("enable_action_bar");
-        this.showingSection.tickRate = configurationSection.getLong("tick_rate");
+        this.showingSection = new ConfigShowingSection(configFile);
     }
 }
