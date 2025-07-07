973a64753f864cd352391c4f6046142748c00456
ilezzov-code <ilezov.morozov@yandex.com>
2025-07-07 21:56:15 +0300

    Add new fields in players table

--- src/main/resources/data/mysql_schema.sql
+++ src/main/resources/data/mysql_schema.sql
@@ -1,4 +1,6 @@
 CREATE TABLE IF NOT EXISTS players (
     uuid CHAR(36) PRIMARY KEY,
-    enable_showing BOOLEAN NOT NULL
+    enable_showing BOOLEAN NOT NULL,
+    enable_action_bar BOOLEAN NOT NULL,
+    enable_boss_bar BOOLEAN NOT NULL
 );
--- src/main/resources/data/sqlite_schema.sql
+++ src/main/resources/data/sqlite_schema.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS players (
     uuid CHAR(36) PRIMARY KEY,
-    enable_showing BOOLEAN NOT NULL
+    display_name VARCHAR(255) NOT NULL,
+    enable_showing BOOLEAN NOT NULL,
+    enable_action_bar BOOLEAN NOT NULL,
+    enable_boss_bar BOOLEAN NOT NULL
 );
